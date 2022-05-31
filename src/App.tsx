110ea21a48a22a2e1e6b2e86435faf987dcbfabd
Lionel <lio4455@hotmail.com>
2022-05-31 12:38:05 +0200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,46 @@
+import { useState } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  const [count, setCount] = useState(0);
+  const [title, setTitle] = useState('');
+
+  async function getData() {
+    try {
+      const response = await fetch(
+        'https://jsonplaceholder.typicode.com/todos/1'
+      );
+
+      if (!response.ok) {
+        throw new Error(`The Error status is: ${response.status}`);
+      }
+
+      const json = await response.json();
+      const title = json.title;
+
+      return title;
+    } catch (error: any) {
+      console.log(error.message);
+    }
+  }
+
+  getData().then((title) => setTitle(title));
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>Hello Vite + React!</p>
+        <p>
+          <button type="button" onClick={() => setCount((count) => count + 1)}>
+            count is: {count}
+          </button>
+        </p>
+        <p>Title fetched from https://jsonplaceholder is : "{title}"</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
